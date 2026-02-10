f0803f8200f8e8c0fa8f58ffee77f73fa7bb39bb
JaimeGDiazUO306054 <uo306054@uniovi.es>
2026-02-10 22:44:48 +0100

    entrega de la practica p11

--- p11/rutalaboratorio.bat
+++ p11/rutalaboratorio.bat
@@ -1,2 +1,2 @@
-set path=C:\Program Files\Java\<jdk>\bin
-set classpath=C:\Users\<loquesea>\<repositorio github>\p11
+set path=C:\Program Files\Eclipse Adoptium\jdk-21.0.8.9-hotspot\bin
+set classpath=C:\Users\jaime\Desktop\alg_GonzalezDiazJaime306054\p11
